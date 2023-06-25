fc35a0e6ef8b79b4be780535aca795d865a6ff42
Nikk45 <72194228+Nikk45@users.noreply.github.com>
2023-06-25 19:51:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+	let chnge = document.getElementsByTagName("strong");
+		for(let i=0;i<chnge.length;i++){
+			chnge[i].style.color = "green";
+		}
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let chnge = document.getElementsByTagName('strong');
+		for(let i=0;i<chnge.length;i++){
+			chnge[i].style.color = "";
+		}
     
 }
